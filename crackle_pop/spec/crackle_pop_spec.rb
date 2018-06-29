329cd93b8dabd8f88dc1d031d24e02deb35ad4a3
marcusfgardiner <marcusfgardiner@gmail.com>
2018-06-29 14:12:42 +0100

    :white_check_mark: test: Crackles when passed 3

---
+++ crackle_pop/spec/crackle_pop_spec.rb
@@ -0,0 +1,12 @@
+require 'crackle_pop'
+
+# Write a program that prints out the numbers 1 to 100 (inclusive). 
+# If the number is divisible by 3, print Crackle instead of the number. 
+# If it's divisible by 5, print Pop. If it's divisible by both 3 and 5, print CracklePop. 
+
+describe 'CracklePop' do
+
+    it 'returns "Crackle" when passed 3' do
+        expect(crackle_pop(3)).to_eq('Crackle')
+    end
+end
