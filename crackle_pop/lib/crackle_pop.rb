e5125ca03d28b03770fd4b249554de4049fb0c1f
marcusfgardiner <marcusfgardiner@gmail.com>
2018-07-06 09:04:55 +0100

    refactor: changed modulus to use .zero? and cleaned up Gemfile

--- crackle_pop/lib/crackle_pop.rb
+++ crackle_pop/lib/crackle_pop.rb
@@ -1,12 +1,16 @@
 def crackle_pop(number)
-  if (number % 3 == 0) && (number % 5) == 0
+  if (number % 3).zero? && (number % 5).zero?
     'CracklePop'
-  elsif (number % 3) == 0
+  elsif (number % 3).zero?
     'Crackle'
-  elsif (number % 5) == 0
+  elsif (number % 5).zero?
     'Pop'
   else number
   end
 end
 
-100.times { |i| puts crackle_pop(i + 1) }
+def printer
+  100.times { |i| puts crackle_pop(i + 1) }
+end
+
+printer
